f89a2b45c9edca17c7d8660bd4322ec59a9d1c6c
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-08-28 22:26:09 -0700

    Create client_example.lua

---
+++ [exmaples]/client_example.lua
@@ -0,0 +1,6 @@
+local  lxCore = exports["lxCore"]:SharedObject() -- fetch the object 
+lxCore.showNotification("d") -- use one of the client side functions
+--lxCore.GetPedMugshot
+--lxCore.GetPlayers
+--lxCore.SpawnVehicle
+-lxCore.GetPeds
