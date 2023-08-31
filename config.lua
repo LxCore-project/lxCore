d939fb5723cf8b798146e2e96e1fcf5ed8afac5d
Alv#9999 <72032190+alvwashere@users.noreply.github.com>
2023-08-31 19:04:20 +0100

    Update config.lua
    added some stuff

--- config.lua
+++ config.lua
@@ -1,3 +1,37 @@
 Config = {}
 
+Config.Locale = 'en' -- The language things will be outputted in. Refer to documentation for available languages.
 Config.IdentifierType = 'license' -- The identifier that will be stored and used to fetch data from in DB. You can use things such as steam, discord, etc. (Refer to documentation to potential issues for not using license).
+
+-- ------------------------------------------------------------------------
+-- ------------------------------------------------------------------------
+
+-- For help with adding/modifying new accounts, refer to documentation.
+
+-- ------------------------------------------------------------------------
+-- ------------------------------------------------------------------------
+
+Config.Accounts = {
+  ['cash'] = {
+    StartingAmount = 50000,
+    Display = {
+      Show = true,
+      Position = 1
+    }
+  },
+  ['bank'] = {
+    StartingAmount = 50000,
+    Display = {
+      Show = true,
+      Position = 2
+    }
+  },
+  ['dirty_money'] = {
+    StartingAmount = 0,
+    Dispaly = {
+      Show = true,
+      Position = 3
+    }
+  },
+}
+
