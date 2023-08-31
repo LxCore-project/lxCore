bca83b0a4bb721f2297f832d2a68f4ff8a8a9035
Alv#9999 <72032190+alvwashere@users.noreply.github.com>
2023-08-31 18:56:55 +0100

    Update fxmanifest.lua
    Add useful stuff since Zaps couldnt

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,14 +1,35 @@
 fx_version 'cerulean'
 games { 'gta5' }
-author 'zaps6000'
 lua54 'yes'
+
+authors 'Zaps6000, AlvWasHere'
+description 'A lightweight LUA 5.4 FiveM Framework'
+url 'https://github.com/LxCore-project/lxCore'
+documentation 'Coming Soon'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
 client_scripts {
-'client/*'
+    'client/*'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-'server/*'
+    'server/*'
 }
 
-dependency 'oxmysql'
+ui_page 'html/index.html'
+files {
+    'html/*.html',
+    'html/css/*.css',
+    'html/js/*.js',
+    'html/img/*'
+}
+
+dependencies {
+    'oxmysql',
+    'ox_lib'
+}
