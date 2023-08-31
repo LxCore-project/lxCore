1ed35785255d70490d5affd62dcbf934ed66bdf2
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-08-31 14:33:30 -0700

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -1,1 +1,12 @@
+CREATE DATABASE IF NOT EXISTS `lxcore`;
+USE `lxcore`;
 
+CREATE TABLE `users` (
+    `identifier` VARCHAR(60) NOT NULL,
+    `job` VARCHAR(20) NULL DEFAULT 'unemployed',
+    `job_grade` INT NULL DEFAULT 0,
+    `location` longtext NULL DEFAULT NULL,
+    `metadata` JSON NULL DEFAULT NULL, 
+
+    PRIMARY KEY (`identifier`)
+) ENGINE=InnoDB;
