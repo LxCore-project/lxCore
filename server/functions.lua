3955a9d6561a4d81b6f78ed6fada48252d06d965
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-08-31 15:21:33 -0700

    Update functions.lua

--- server/functions.lua
+++ server/functions.lua
@@ -1,5 +1,18 @@
 LxCore = {}
 
+function LxCore.GetPlayerIdentifier(type, player)
+    local id = GetNumPlayerIdentifiers(player)
+    for count = 0, id do
+        local identifier = GetPlayerIdentifier(player, count)
+        if identifier and string.find(identifier, type) then
+            return identifier
+        end
+    end
+    return nil
+end
+
+
+
 exports('SharedObject', function()
 	return LxCore
 end)
