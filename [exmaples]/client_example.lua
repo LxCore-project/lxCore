2188e16cb5c8cc91840cb844a6c68e1ed9be2a27
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-08-31 15:34:06 -0700

    Update client_example.lua

--- [exmaples]/client_example.lua
+++ [exmaples]/client_example.lua
@@ -1,4 +1,4 @@
 local  lxCore = exports["lxCore"]:SharedObject() -- fetch the object 
 -- @param message string The message to display
-lxCore.showNotification("d") -- use one of the client side functions
+lxCore.showNotification("EXAMPLE EXAMPLE") -- use one of the client side functions
 LxCore.GetPlayers()
