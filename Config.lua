4120b54621862aee1473185af32a4fdbce29105b
StrixLuca <110634650+StrixLuca@users.noreply.github.com>
2023-08-31 19:45:37 +0200

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -4,3 +4,4 @@ Config = {}
 -- Notify system
 Config.chatnotify = true -- Enable chat notify (if false, set Config.oxnotify to true)
 Config.oxnotify = false  -- Enable ox_lib notify
+Config.NotifyPosition = 'top-right' -- 'top' | 'top-right' | 'top-left' | 'bottom' | 'bottom-right' | 'bottom-left' for ox_lib
