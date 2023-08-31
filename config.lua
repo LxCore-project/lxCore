1891b32fc00dc44c9b0fc01a60f09a040b5608ec
Alv#9999 <72032190+alvwashere@users.noreply.github.com>
2023-08-31 18:50:32 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,3 +1,3 @@
 Config = {}
 
-Config.IdentifierType = 'license' -- You can use things such as steam, discord, etc. (Refer to documentation to potential issues for not using license).
+Config.IdentifierType = 'license' -- The identifier that will be stored and used to fetch data from in DB. You can use things such as steam, discord, etc. (Refer to documentation to potential issues for not using license).
