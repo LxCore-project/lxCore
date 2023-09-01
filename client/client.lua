840090e9546b40f56b9b0c9096642041bc1694dd
StrixLuca <110634650+StrixLuca@users.noreply.github.com>
2023-09-01 13:03:05 +0200

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,8 +1,22 @@
+local PlayerData = {}
+
+-- Register the network event and add the data to PlayerData
 RegisterNetEvent('lxcore:client:loaded')
 AddEventHandler('lxcore:client:loaded', function(Player) 
 	LxCore.PlayerData = Player
 
 
 		end)
-	
-    
+
+-- Function to set player data in PlayerData
+function SetPlayerData(Player)
+    PlayerData = Player
+end
+
+-- Function to get player data
+function GetPlayerData()
+    return PlayerData
+end
+
+-- Export the GetPlayerData function
+exports('getplayer', GetPlayerData)           ----local playerData = exports['lxCore']:getplayer()
