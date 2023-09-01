6d2a3addca68c108fc1006950beacf6f8bec3880
Zaps6000 <122732007+Zaps6000@users.noreply.github.com>
2023-08-31 19:13:37 -0700

    Update callback.lua

--- server/modules/callback.lua
+++ server/modules/callback.lua
@@ -1,4 +1,5 @@
 local calls = {}
+local pendingClientRequests = {}
 
 ---@param eventName string
 ---@param callback function
@@ -18,4 +19,37 @@ AddEventHandler('lxcore:triggerServerCallback', function(eventName, requestId, i
     calls[eventName](source, function(...)
         TriggerClientEvent('lxcore:serverCallback', source, requestId, invoker, ...)
     end)
+end)
+
+
+
+local function generateRequestId()
+  local newRequestId = #pendingClientRequests + 1
+  return newRequestId
+end
+
+---@param player number The server ID of the player
+---@param eventName string The name of the event to be triggered
+---@param callback function The callback function to execute
+LxCore.TriggerClientCallback = function(player, eventName, callback, ...)
+  if type(player) ~= 'number' or type(eventName) ~= 'string' or type(callback) ~= 'function' then
+    return print('LxCore ERROR: Invalid parameters passed to TriggerClientCallback')
+  end
+
+  local requestId = generateRequestId()
+  pendingClientRequests[requestId] = callback
+
+  TriggerClientEvent('lxcore:triggerClientCallback', player, eventName, requestId, GetInvokingResource() or "unknown", ...)
+end
+
+RegisterNetEvent('lxcore:clientCallback')
+AddEventHandler('lxcore:clientCallback', function(requestId, invoker, ...)
+  local callback = pendingClientRequests[requestId]
+  
+  if callback then
+    callback(...)
+    pendingClientRequests[requestId] = nil -- Remove completed request
+  else
+    print(string.format('[LxCore ERROR!!!!!] Client Callback with requestId %s was called by %s but does not exist.', requestId, invoker))
+  end
 end)
