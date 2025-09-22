411672f4f95a9de6004d37e51a83136c4d6069f5
Alv <72032190+alvwashere@users.noreply.github.com>
2025-09-22 16:33:18 +0100

    Update client.lua
    got bored thought lets do something to this weak project lol

--- client/client.lua
+++ client/client.lua
@@ -1,12 +1,34 @@
-local defaultCashAccount = Config.Accounts['cash'] 
-local defaultBankAccount = Config.Accounts['bank'] 
+local LxCore = {}
+
+local LxCore.defaultCashAccount = Config.Accounts['cash'] 
+local LxCore.defaultBankAccount = Config.Accounts['bank'] 
 
 
 RegisterNetEvent('lxcore:client:loaded')
 AddEventHandler('lxcore:client:loaded', function(Player) 
-	LxCore.PlayerData = Player
+	local LxCore.PlayerData = Player or {}
 
+	-- load data, skin, accounts, job
 
-		end)
-	
-    
+	if LxCore.PlayerData.cash and LxCore.PlayerData.bank and LxCore.PlayerData.job_label and LxCore.PlayerData.grade_label then
+		LxCore.UpdateNUI('money_hud', LxCore.PlayerData)
+	end
+
+	if LxCore.PlayerData.skin then
+		
+	end
+
+	-- 
+end)
+
+local function LxCore.UpdateNUI(data)
+	if not data then return print('[ERROR-05] Couldn\'t load contents of player data table. HUD cannot display.')
+		
+	SendNUIMessage({
+		type = 'update_mhud',
+		money = data.money,
+		bank = data.bank,
+		job = data.job_label,
+		grade = data.grade_label
+	})
+end
